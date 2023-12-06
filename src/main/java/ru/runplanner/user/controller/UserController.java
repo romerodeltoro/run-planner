0946bb4f778e0feb3ec758cbeabc2954d07f6bdd
Yura Iskhakov <romerodeltoro@yandex.ru>
2023-12-06 21:31:01 +0300

    feat: add security

--- src/main/java/ru/runplanner/user/controller/UserController.java
+++ src/main/java/ru/runplanner/user/controller/UserController.java
@@ -4,17 +4,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.runplanner.user.dto.UserDto;
 import ru.runplanner.user.service.UserService;
 
 @Validated
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/api/v1/user")
 public class UserController {
 
     private final UserService userService;
@@ -22,6 +19,16 @@ public class UserController {
     @PostMapping
     public ResponseEntity<UserDto> creatUser(@Valid @RequestBody UserDto user) {
         return ResponseEntity.ok().body(userService.createUser(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
+        return ResponseEntity.ok().body(userService.getUser(id));
+    }
+
+    @GetMapping
+    public String get() {
+        return "Hello!";
     }
 
 }
--- src/main/java/ru/runplanner/user/service/UserService.java
+++ src/main/java/ru/runplanner/user/service/UserService.java
@@ -3,8 +3,11 @@ package ru.runplanner.user.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.exceptions.AlreadyInitializedException;
 import ru.runplanner.user.dto.UserDto;
+import ru.runplanner.user.exception.NotFoundException;
 import ru.runplanner.user.mapper.UserMapper;
 import ru.runplanner.user.model.User;
 import ru.runplanner.user.storage.UserRepository;
@@ -17,8 +20,32 @@ public class UserService {
     private final UserRepository userRepository;
 
     public UserDto createUser(UserDto userDto) {
+        if (userRepository.findByEmail(UserMapper.INSTANCE.toUser(userDto).getEmail()) != null) {
+            throw new AlreadyInitializedException(String.format(
+                    "Пользователь с электронной почтой %s уже зарегистрирован.", userDto.getEmail()
+            ));
+        }
         User user = userRepository.save(UserMapper.INSTANCE.toUser(userDto));
         log.info("Создан новый пользователь: '{}'", user);
         return UserMapper.INSTANCE.toUserDto(user);
+    }
+
+    public String addUser(UserDto userDto) {
+        if (userRepository.findByEmail(UserMapper.INSTANCE.toUser(userDto).getEmail()) != null) {
+            throw new AlreadyInitializedException(String.format(
+                    "Пользователь с электронной почтой %s уже зарегистрирован.", userDto.getEmail()
+            ));
+        } else {
+            return null;
+        }
+    }
+
+    public UserDto getUser(Long id) {
+        UserDto userDto = UserMapper.INSTANCE.toUserDto(userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(
+                        String.format("Пользователя с id=%d нет в базе", id)
+                )));
+        log.info("Получен пользователь {}", userDto);
+        return userDto;
     }
 }
